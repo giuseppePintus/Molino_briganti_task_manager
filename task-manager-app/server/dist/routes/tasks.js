8df4fe24128c233f749011a4fbf2bf5d968fd4fb
cippo <manuel.maldini.70@gmail.com>
2025-12-07 18:49:15 +0100

    poi

--- task-manager-app/server/dist/routes/tasks.js
+++ task-manager-app/server/dist/routes/tasks.js
@@ -18,6 +18,8 @@ router.post('/:id/pause', (req, res) => tasksController.pauseTask(req, res));
 router.post('/:id/resume', (req, res) => tasksController.resumeTask(req, res));
 router.post('/:id/postpone', (req, res) => tasksController.postponeTask(req, res));
 router.post('/:id/reset-suspended', (req, res) => tasksController.resetTaskToSuspended(req, res));
+// Task management
+router.post('/move-overdue', (req, res) => tasksController.moveOverdueTasks(req, res));
 // Task notes routes
 router.post('/:id/notes', (req, res) => tasksController.addNote(req, res));
 router.get('/:id/notes', (req, res) => tasksController.getNotes(req, res));
